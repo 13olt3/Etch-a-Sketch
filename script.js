7eb9cea149ba1f37b868f4dec731460fa0a6244a
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-07-07 13:55:59 +1000

    Changed used for loop to create squares

--- script.js
+++ script.js
@@ -1,155 +1,16 @@
+
 const container = document.querySelector(".container");
 
-const div1 = document.createElement("div");
-const div2 = document.createElement("div");
-const div3 = document.createElement("div");
-const div4 = document.createElement("div");
-const div5 = document.createElement("div");
-const div6 = document.createElement("div");
-const div7 = document.createElement("div");
-const div8 = document.createElement("div");
-const div9 = document.createElement("div");
-const div10 = document.createElement("div");
-const div11 = document.createElement("div");
-const div12 = document.createElement("div");
-const div13 = document.createElement("div");
-const div14 = document.createElement("div");
-const div15 = document.createElement("div");
-const div16 = document.createElement("div");
-
-div1.classList.add("box");
-div2.classList.add("box");
-div3.classList.add("box");
-div4.classList.add("box");
-div5.classList.add("box");
-div6.classList.add("box");
-div7.classList.add("box");
-div8.classList.add("box");
-div9.classList.add("box");
-div10.classList.add("box");
-div11.classList.add("box");
-div12.classList.add("box");
-div13.classList.add("box");
-div14.classList.add("box");
-div15.classList.add("box");
-div16.classList.add("box");
-
-container.appendChild(div1);
-container.appendChild(div2);
-container.appendChild(div3);
-container.appendChild(div4);
-container.appendChild(div5);
-container.appendChild(div6);
-container.appendChild(div7);
-container.appendChild(div8);
-container.appendChild(div9);
-container.appendChild(div10);
-container.appendChild(div11);
-container.appendChild(div12);
-container.appendChild(div13);
-container.appendChild(div14);
-container.appendChild(div15);
-container.appendChild(div16);
-
-const subDiv11 = document.createElement("div");
-const subDiv12 = document.createElement("div");
-const subDiv13 = document.createElement("div");
-const subDiv14 = document.createElement("div");
-const subDiv15 = document.createElement("div");
-const subDiv16 = document.createElement("div");
-const subDiv17 = document.createElement("div");
-const subDiv18 = document.createElement("div");
-const subDiv19 = document.createElement("div");
-const subDiv110 = document.createElement("div");
-const subDiv111 = document.createElement("div");
-const subDiv112 = document.createElement("div");
-const subDiv113 = document.createElement("div");
-const subDiv114 = document.createElement("div");
-const subDiv115 = document.createElement("div");
-
-subDiv11.classList.add("sub1");
-subDiv12.classList.add("sub1");
-subDiv13.classList.add("sub1");
-subDiv14.classList.add("sub1");
-subDiv15.classList.add("sub1");
-subDiv16.classList.add("sub1");
-subDiv17.classList.add("sub1");
-subDiv18.classList.add("sub1");
-subDiv19.classList.add("sub1");
-subDiv110.classList.add("sub1");
-subDiv111.classList.add("sub1");
-subDiv112.classList.add("sub1");
-subDiv113.classList.add("sub1");
-subDiv114.classList.add("sub1");
-subDiv115.classList.add("sub1");
-
-div1.appendChild(subDiv11);
-div1.appendChild(subDiv12);
-div1.appendChild(subDiv13);
-div1.appendChild(subDiv14);
-div1.appendChild(subDiv15);
-div1.appendChild(subDiv16);
-div1.appendChild(subDiv17);
-div1.appendChild(subDiv18);
-div1.appendChild(subDiv19);
-div1.appendChild(subDiv110);
-div1.appendChild(subDiv111);
-div1.appendChild(subDiv112);
-div1.appendChild(subDiv113);
-div1.appendChild(subDiv114);
-div1.appendChild(subDiv115);
-
-const subDiv21 = document.createElement("div");
-const subDiv22 = document.createElement("div");
-const subDiv23 = document.createElement("div");
-const subDiv24 = document.createElement("div");
-const subDiv25 = document.createElement("div");
-const subDiv26 = document.createElement("div");
-const subDiv27 = document.createElement("div");
-const subDiv28 = document.createElement("div");
-const subDiv29 = document.createElement("div");
-const subDiv210 = document.createElement("div");
-const subDiv211 = document.createElement("div");
-const subDiv212 = document.createElement("div");
-const subDiv213 = document.createElement("div");
-const subDiv214 = document.createElement("div");
-const subDiv215 = document.createElement("div");
-
-subDiv21.classList.add("sub1");
-subDiv22.classList.add("sub1");
-subDiv23.classList.add("sub1");
-subDiv24.classList.add("sub1");
-subDiv25.classList.add("sub1");
-subDiv26.classList.add("sub1");
-subDiv27.classList.add("sub1");
-subDiv28.classList.add("sub1");
-subDiv29.classList.add("sub1");
-subDiv210.classList.add("sub1");
-subDiv211.classList.add("sub1");
-subDiv212.classList.add("sub1");
-subDiv213.classList.add("sub1");
-subDiv214.classList.add("sub1");
-subDiv215.classList.add("sub1");
-
-div2.appendChild(subDiv21);
-div2.appendChild(subDiv22);
-div2.appendChild(subDiv23);
-div2.appendChild(subDiv24);
-div2.appendChild(subDiv25);
-div2.appendChild(subDiv26);
-div2.appendChild(subDiv27);
-div2.appendChild(subDiv28);
-div2.appendChild(subDiv29);
-div2.appendChild(subDiv210);
-div2.appendChild(subDiv211);
-div2.appendChild(subDiv212);
-div2.appendChild(subDiv213);
-div2.appendChild(subDiv214);
-div2.appendChild(subDiv215);
-
-
-
-
+for ( var i = 0; i < 16; i++){
+    var squareColumn = document.createElement("div");
+    squareColumn.setAttribute("class", "box");
+    for( var j = 0; j < 15; j++){
+        var squareRow = document.createElement("div");
+        squareRow.setAttribute("class", "sub1");
+        squareColumn.appendChild(squareRow);
+    }
+    container.appendChild(squareColumn);
+}
 
 
 
