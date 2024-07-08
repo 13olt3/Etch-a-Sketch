26e81660bf01b4e8a45f5a65012a1647d349221a
13olt3 <31859433+13olt3@users.noreply.github.com>
2024-07-08 17:57:53 +1000

    Added resize button functionality

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ in each div row create a child container
 
 create event listener when mouse leaves box, change box color
 
+creating size of the grid
+default grid size is (200px * 200px)
+16 squares by 16 square is default
+so square width and height is math floor 200/16 px;
+
+finished adding grid adding button functionality. need to remove grid before adding a new one
+change width and height of tiles to equal 200 / no of tiles px
--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 const container = document.querySelector(".container");
+const body = document.querySelector("body");
+
+const button = document.createElement("button");
+button.textContent = "Resize grid";
+button.addEventListener("click",getGridSize);
+body.appendChild(button);
+let tiles = 16;
 
 
-for (let i=0; i <16; ++i){
-    var newBox = document.createElement("div");
-    newBox.setAttribute("class", "row");   
+function getGridSize() {
     
-    for (let j=0; j<16; ++j){
-        var boxRow = document.createElement("div");
-        boxRow.setAttribute("class", "box");
-        newBox.appendChild(boxRow);
+    var userInput = prompt("Enter desired grid size: ");
+    while ((userInput <= 0) || (userInput >= 100)) {
+        userInput = prompt("Invalid input. Choose a number between (0 - 100).");
     }
-    container.appendChild(newBox);
+    
+    createGrid(userInput);
+    return;
+
+}
+let tileWidth = 200 / tiles;
+let tileHeight = 200 / tiles;
+
+function createGrid(numOfTiles){
+    container.replaceChildren();
+    for (let i=0; i <numOfTiles; ++i){
+        var newBox = document.createElement("div");
+        newBox.setAttribute("class", "row");   
+        
+        for (let j=0; j<numOfTiles; ++j){
+            var boxRow = document.createElement("div");
+            boxRow.setAttribute("class", "box");
+            boxRow.setAttribute("style", "width: "+(200 / numOfTiles)+"px; height: " +(200 / numOfTiles)+ "px;");
+            
+            
+    
+            
+            newBox.appendChild(boxRow);
+        }
+        const allBoxes = document.querySelectorAll(".box");
+        allBoxes.forEach((button) => {
+            button.addEventListener("mouseover", function(e){
+                e.target.style.background = "red";        
+            });
+        });
+        container.appendChild(newBox);
+    }
+
 }
 
-// box.addEventListener("mouseover", function(e){
-//     e.target.style.background = "blue";
-// });
-const allBoxes = document.querySelectorAll(".box");
-allBoxes.forEach((button) => {
-    button.addEventListener("mouseover", function(e){
 
-        e.target.style.background = "red";
-        
-    });
-});
+createGrid(16);
+
+
+
+
+
+// const allBoxes = document.querySelectorAll(".box");
+// allBoxes.forEach((button) => {
+//     button.addEventListener("mouseover", function(e){
+//         e.target.style.background = "red";        
+//     });
+// });
+
